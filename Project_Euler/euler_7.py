113dc0794bc243c46f4cd39d40040c199193b635
RainyRoot <158036398+RainyRoot@users.noreply.github.com>
2025-02-12 05:21:00 +0100

    Create euler_7.py

---
+++ Project_Euler/euler_7.py
@@ -0,0 +1,27 @@
+# Problem 7: 10001st prime number
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def nth_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+prime_position = 10001
+
+prime_result = nth_prime(prime_position)
+print(f"Problem 7: The {prime_position}st prime number is: {prime_result}")
