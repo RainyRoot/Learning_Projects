f6f3fe2bda394756e1004abddd7ab69be076ec92
RainyRoot <158036398+RainyRoot@users.noreply.github.com>
2025-02-12 05:21:09 +0100

    Update euler_6.py

--- Project_Euler/euler_6.py
+++ Project_Euler/euler_6.py
@@ -1,4 +1,5 @@
-# Problem 6: Difference between sum of squares and square of sum
+# Problem 6: Difference between sum of squares and square of 
+
 def sum_square_difference(n):
     sum_of_squares = sum(i ** 2 for i in range(1, n + 1))
     square_of_sum = sum(range(1, n + 1)) ** 2
