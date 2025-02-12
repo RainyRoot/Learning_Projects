12a4005f71a514b0bb954c8fb7a2f94016c60a73
RainyRoot <158036398+RainyRoot@users.noreply.github.com>
2025-02-12 05:09:52 +0100

    Create euler_1.py

---
+++ Project_Euler/euler_1.py
@@ -0,0 +1,11 @@
+def sum_of_multiples(limit, multiples):
+    return sum(
+        number for number in range(limit)
+        if any(number % m == 0 for m in multiples)
+    )
+
+limit = 1000
+multiples = [3, 5]
+
+result = sum_of_multiples(limit, multiples)
+print(f"The sum of all multiples of {multiples} below {limit} is: {result}")
